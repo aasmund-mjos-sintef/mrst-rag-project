8e9ffbcf5e066626754a29d06a99b1cddd35034c
aasmund-mjos-sintef <Aasmund.mjos@sintef.no>
2025-07-10 14:55:12 +0200

    vector graphics show book graph

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import streamlit as st
 from graph import *
 import io
+from PIL import Image
+from streamlit_image_zoom import image_zoom
+import streamlit.components.v1 as components
 
 st.set_page_config(layout = "wide", page_title = "MRST Assistant", page_icon = "mrst_logo.png")
 
@@ -19,7 +22,7 @@ if "response" not in st.session_state:
     st.session_state.response = ""
 
 if "figures" not in st.session_state:
-    st.session_state.figures = [""]
+    st.session_state.figures = []
 
 def run_graph():
     query = st.session_state.query
@@ -30,25 +33,47 @@ def run_graph():
     st.session_state.response = state.get('response')
 
     figures = state.get('figures')
-    svg_figures = []
+    images = []
 
     if figures != None:
+        print("Found ", len(figures), " chapters!")
         for fig in figures:
+
             buf = io.StringIO()
-            fig.savefig(buf, format = "svg")
-            svg_figures.append(buf.getvalue())
+            fig.savefig(buf, format = 'svg')
+            image = buf.getvalue()
+            images.append(image)
             buf.close()
 
-    st.session_state.figures = svg_figures
+    st.session_state.figures = images
 
-
-query = st.text_input("What can I help you with? ", "", key = "query", on_change=run_graph)
+information_area = st.markdown("## What can I help you with?")
+query = st.text_input("", "", key = "query", on_change=run_graph)
 response_area = st.text("")
-response_figures = st.markdown("")
 response_area.text(st.session_state.response)
 
-svg_content = ""
-for svg_fig in st.session_state.figures:
-    svg_content += svg_fig
-
-response_figures.markdown(svg_content, unsafe_allow_html=True)
+for img in st.session_state.figures:
+    components.html("""
+<div style="
+    border: 2px solid #ccc;
+    border-radius: 8px;
+    padding: 10px;
+    margin-bottom: 20px;
+    background-color: #ffffff;
+    display: inline-block;
+    box-sizing: border-box;
+    min-height: 100px;
+">
+    <div id="svg-container" style="width: auto; height: auto; overflow: auto;">
+        """ + img + """
+    </div>
+    <script src="https://unpkg.com/@panzoom/panzoom/dist/panzoom.min.js"></script>
+    <script>
+        var elem = document.getElementById('svg-container');
+        var panzoom = Panzoom(elem, { maxScale: 5, minScale: 1});
+        elem.addEventListener('wheel', panzoom.zoomWithWheel);
+    </script>
+</div>
+""", height = 550)
+    
+    st.markdown("Some Chapter")
