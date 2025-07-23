a6725739e15161825e63126f6298577ce7e9bcac
aasmund-mjos-sintef <Aasmund.mjos@sintef.no>
2025-07-23 17:23:55 +0200

    added lots of journal scrapings

--- generate_papers_dataset.py
+++ generate_papers_dataset.py
@@ -2,16 +2,14 @@ import requests
 from bs4 import BeautifulSoup
 from pypdf._reader import PdfReader
 import io
-import time
 from typing import List
 
-start_links = ["https://www.sintef.no/projectweb/mrst/publications/papers-by-mrst-team/",
-               "https://www.sintef.no/projectweb/mrst/publications/proceedings-by-mrst-team/",
-               "https://www.sintef.no/projectweb/mrst/publications/papers-by-others/",
-               "https://www.sintef.no/projectweb/mrst/publications/proceedings-papers/",
-               "https://www.sintef.no/projectweb/mrst/publications/papers-by-others2/",
-               "https://www.sintef.no/projectweb/mrst/publications/phd-theses/",
-               "https://www.sintef.no/projectweb/mrst/publications/master-theses/"]
+from dotenv import load_dotenv
+import os
+load_dotenv()
+elsevier_api_key = os.getenv("ELSEVIER_API_KEY")
+
+###### EVERYTHING THAT IS RETURNED AS NONE IN THE TRY FETCHING DEFINITION HAS TO BE IMPLEMENTED IN A NICE WAY TO GATHER THE DATA AND CORRECT DATA FORMAT
 
 def split_by_more(string: str, chars: List[str] = []):
     if chars == []:
@@ -42,61 +40,241 @@ def get_authors(og_line):
     names = split_by_more(name_line,[",and","and",","])
     return names, title, name_line
 
+visited = set()
 
-links = []
-authors = {}
-titles = []
-paper_authors = []
+def try_fetching_pdf(url, only_abstract):
 
-for mrst_link in start_links:
-    response = requests.get(mrst_link)
-    soup = BeautifulSoup(response.content, 'html.parser')
-    for section in soup.find_all('div', class_ = "rich-text-field"):
-            for article in section.find_all("li"):
-                for link_box in article.find_all('a'):
-                    paper_link = link_box.get('href')
-                    if paper_link != None:
-                        if 'http' in paper_link:
-                            links.append(paper_link)
-                        else:
-                            links.append('https://www.sintef.no' + paper_link)
+    try:
+        response = requests.get(url)
+        r_url = response.url
+        https = "https://"
+        url_snippet = r_url[len(https):]
+        index = url_snippet.find('/')
+        url_snippet = url_snippet[:index]
 
-                        line = article.get_text()
-                        a, title, name_line = get_authors(line)
-                        this_paper_authors = []
-                        for single_a in a:
-                            single_a = single_a.replace(" ","")
-                            split = split_by_more(single_a.strip(), [".","-"])
-                            split = [s for s in split if s not in [""," ","  "]]
-                            if len(split)>=2:
-                                single_a = split[0] + "." + split[-1]
-                                authors[single_a] = authors.get(single_a,0) + 1
-                                this_paper_authors.append(single_a)
+        ctype = response.headers.get("Content-Type")
+        if "pdf" in ctype:
+            try:
+                on_fly_mem_obj = io.BytesIO(response.content)
+                return PdfReader(on_fly_mem_obj)
+            except:
+                pass
 
-                        paper_authors.append(this_paper_authors)
+    except Exception as e:
+        return None
 
-                        titles.append(title)
+    actually_try = True
+    nice_one = True
 
-def try_fetching_pdf(link):
-    response = requests.get(link)
-    ctype = response.headers.get("Content-Type")
-    if "pdf" in ctype:
-        try:
-            on_fly_mem_obj = io.BytesIO(response.content)
-            return PdfReader(on_fly_mem_obj)
-        except:
-            pass
+    if "linkinghub.elsevier.com" == url_snippet:
 
-    url = response.url
+        if actually_try:
 
-    # Gotta make methods to parse different journals into pdfs
+            if only_abstract:
 
-    if "ager" in url:       # Advanced in Geo-Energy research
+                https_doi = "https://doi.org"
+                doi = url[len(https_doi):]
+                elsevier_url = "https://api.elsevier.com/content/article/doi/" + doi + "?view=META_ABS"
+                response = requests.get(elsevier_url, headers={"X-ELS-APIKey": elsevier_api_key})
+                soup = BeautifulSoup(response.content, 'html.parser')
+                abstract = soup.get_text().split("\n")[1].strip()
+                print(abstract)
+
+            else:
+
+                # THIS ONE DOES NOT WORK ATM
+
+                https_doi = "https://doi.org"
+                doi = url[len(https_doi):]
+                elsevier_url = "https://api.elsevier.com/content/article/doi/" + doi + "?view=FULL"
+                response = requests.get(elsevier_url, headers={"X-ELS-APIKey": elsevier_api_key})
+                print(response.content)
+
+    elif "onepetro.org" == url_snippet:
+
+        if actually_try:
+
+            if only_abstract:
+
+                # SHOULD WAIT FOR LEGAL THINGS
+
+                from selenium.webdriver import Firefox
+                from selenium.webdriver.firefox.options import Options
+                from selenium.webdriver.firefox.service import Service
+
+                firefox_path = r"/home/aasmund_mjos/firefox_mozilla/firefox"
+                geckodriver_path = r"/home/aasmund_mjos/Downloads/geckodriver-v0.36.0-linux64/geckodriver"
+
+                service = Service(geckodriver_path)
+                options = Options()
+                options.binary_location = firefox_path
+
+                driver = Firefox(service = service, options=options)
+                driver.get(r_url)
+                html = driver.page_source
+
+                soup = BeautifulSoup(html, 'html.parser')
+                abstract_box = soup.find("section", class_ = 'abstract')
+                abstract = ""
+                for p in abstract_box.find_all('p'):
+                    abstract += p.get_text()
+                    abstract += "\n"
+                print(abstract)
+
+                driver.close()
+
+            else:
+                pass
+
+    elif "link.springer.com	" == url_snippet:
         pass
 
-    if "springer" in url:
+    elif "www.earthdoc.org" == url_snippet:
+        if actually_try:
+            if only_abstract:
+                headers = {
+                    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)",
+                    "Accept": "text/html",
+                }
+                response = requests.get(r_url, headers=headers)
+                soup = BeautifulSoup(response.content, 'html.parser')
+                abstract = ""
+                abstract_box = soup.find("div", class_ = "description")
+                for p in abstract_box.find_all('p'):
+                    abstract += p.get_text()
+                    abstract += "\n"
+                print(abstract)
+            else:
+                pass
+
+    elif "andreas.folk.ntnu.no" == url_snippet:
+        pass
+
+    elif "www.mdpi.com" == url_snippet:
+        if actually_try:
+            if only_abstract:
+                pass
+            else:
+
+                headers = {
+                    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)",
+                    "Accept": "text/html",
+                }
+                response = requests.get(r_url + '/pdf', headers=headers)
+                ctype = response.headers.get("Content-Type")
+                if "pdf" in ctype:
+                    on_fly_mem_obj = io.BytesIO(response.content)
+                    reader = PdfReader(on_fly_mem_obj)
+                    return reader
+                else:
+                    return None
+        pass
+
+    elif "agupubs.onlinelibrary.wiley.com" == url_snippet:
+
+        if actually_try:
+            if only_abstract:
+                from selenium.webdriver import Firefox
+                from selenium.webdriver.firefox.options import Options
+                from selenium.webdriver.firefox.service import Service
+
+                firefox_path = r"/home/aasmund_mjos/firefox_mozilla/firefox"
+                geckodriver_path = r"/home/aasmund_mjos/Downloads/geckodriver-v0.36.0-linux64/geckodriver"
+
+                service = Service(geckodriver_path)
+                options = Options()
+                options.binary_location = firefox_path
+
+                driver = Firefox(service = service, options=options)
+                driver.get(r_url)
+                html = driver.page_source
+                soup = BeautifulSoup(html, 'html.parser')
+
+                abstract = ""
+
+                for p in soup.find_all('p'):
+                    span = p.find('span', class_='paraNumber')
+                    if span:
+                        abstract = (p.get_text())
+                        break
+
+                print(abstract)
+                        
+                driver.quit()
+            else:
+                from selenium.webdriver import Firefox
+                from selenium.webdriver.firefox.options import Options
+                from selenium.webdriver.firefox.service import Service
+
+                firefox_path = r"/home/aasmund_mjos/firefox_mozilla/firefox"
+                geckodriver_path = r"/home/aasmund_mjos/Downloads/geckodriver-v0.36.0-linux64/geckodriver"
+
+                service = Service(geckodriver_path)
+                options = Options()
+                options.binary_location = firefox_path
+
+                driver = Firefox(service = service, options=options)
+                driver.get(r_url)
+                html = driver.page_source
+                soup = BeautifulSoup(html, 'html.parser')
+
+                paragraphs = []
+
+                for p in soup.find_all('p'):
+                    span = p.find('span', class_='paraNumber')
+                    if span:
+                        paragraphs.append(p.get_text())
+
+                print(paragraphs)
+
+                driver.quit()
+
+    elif "arxiv.org" == url_snippet:
+        pass
+
+    elif "ager.yandypress.com" == url_snippet:
+
+        if actually_try:
+            if only_abstract:
+
+                soup = BeautifulSoup(response.content, 'html.parser')
+                abstract = ""
+                abstract_box = soup.find('section', class_ = "item abstract")
+                for p in abstract_box.find_all('p'):
+                    if not "document type:" in p.get_text().lower():
+                        abstract += p.get_text()
+                        abstract += "\n"
+                    else:
+                        break
+                print(abstract)
+            else:
+                soup = BeautifulSoup(response.content, 'html.parser')
+                for element in soup.find_all(href = True):
+                    if "PDF" in element.get_text():
+                        link = element.get('href')
+                        response = requests.get(link)
+                        ctype = response.headers.get("Content-Type")
+                        soup = BeautifulSoup(response.content, 'html.parser')
+                        for element in soup.find_all('a', class_ = "download"):
+                            download_link = element.get('href')
+                            response = requests.get(download_link)
+                            ctype = response.headers.get("Content-Type")
+                            if "pdf" in ctype:
+                                try:
+                                    on_fly_mem_obj = io.BytesIO(response.content)
+                                    return PdfReader(on_fly_mem_obj)
+                                except:
+                                    return None
+
+    else:
+        nice_one = False
         pass
     
+    if nice_one:
+        if url_snippet not in visited:
+            print(url_snippet, ":   ", url)
+            visited.add(url_snippet)
+
     return None
     
 def extract_titles_from_outline(outline):
@@ -107,53 +285,108 @@ def extract_titles_from_outline(outline):
         else:
             result.append(" ".join(item.title.lower().split()))
     return result
-    
-x = len(links)
-succesfull = 0
 
-for i, (url, a, t) in enumerate(zip(links, paper_authors, titles)):
-    print(f"Working with paper nr. {i+1} out of {x}", end = "\r")
-    reader = try_fetching_pdf(url)
-    if reader != None:
-        outline = reader.outline
+def read_pdfs(reader):
 
-        sections = extract_titles_from_outline(outline)
+    # THIS ONE DOESN'T RETURN ANYTHING AS OF NOW
+        
+    outline = reader.outline
+    sections = extract_titles_from_outline(outline)
+    total_text = ""
 
-        total_text = ""
+    for page in reader.pages:
+        page_text = " ".join(page.extract_text().lower().split())
+        total_text += page_text
 
-        for page in reader.pages:
-            page_text = " ".join(page.extract_text().lower().split())
-            total_text += page_text
+    indexes = []
+    for i, sec in enumerate(sections):
+        if i == 0:
+            start_index = 0
+        else:
+            start_index = indexes[i-1]
+        
+        indexes.append(total_text.find(sec, start_index))
 
-        indexes = []
-        for i, sec in enumerate(sections):
-            if i == 0:
-                start_index = 0
-            else:
-                start_index = indexes[i-1]
-            
-            indexes.append(total_text.find(sec, start_index))
+    chapters = []
 
-        chapters = []
+    for i_start, i_end, sec in zip(indexes, indexes[1:], sections):
+        s = total_text[i_start:i_end]
+        s = s.replace(sec, sec + ":\n\n", count = 1)
+        try:
+            n = len(s)
+            for i in range(2,10):
 
-        for i_start, i_end, sec in zip(indexes, indexes[1:], sections):
-            s = total_text[i_start:i_end]
-            s = s.replace(sec, sec + ":\n\n", count = 1)
-            try:
-                n = len(s)
-                for i in range(2,10):
+                if s[n-i] == " ":
+                    num_sec = s[n-i:]
+                    s = s.replace(num_sec, "")
+                    break
+        except:
+            pass
 
-                    if s[n-i] == " ":
-                        num_sec = s[n-i:]
-                        s = s.replace(num_sec, "")
-                        break
-            except:
-                pass
+        chapters.append(s)
 
-            chapters.append(s)
+def generate_dataset(only_abstract = False):
 
-        succesfull += 1
+    start_links = ["https://www.sintef.no/projectweb/mrst/publications/papers-by-mrst-team/",
+               "https://www.sintef.no/projectweb/mrst/publications/proceedings-by-mrst-team/",
+               "https://www.sintef.no/projectweb/mrst/publications/papers-by-others/",
+               "https://www.sintef.no/projectweb/mrst/publications/proceedings-papers/",
+               "https://www.sintef.no/projectweb/mrst/publications/papers-by-others2/",
+               "https://www.sintef.no/projectweb/mrst/publications/phd-theses/",
+               "https://www.sintef.no/projectweb/mrst/publications/master-theses/"]
 
-    if i%10==0:
-        print(f"{succesfull} successfull out of {i+1}", end = "\r")
-        time.sleep(5)
+    links = []
+    authors = {}
+    titles = []
+    paper_authors = []
+
+    for mrst_link in start_links:
+        response = requests.get(mrst_link)
+        soup = BeautifulSoup(response.content, 'html.parser')
+        for section in soup.find_all('div', class_ = "rich-text-field"):
+                for article in section.find_all("li"):
+                    for link_box in article.find_all('a'):
+                        paper_link = link_box.get('href')
+                        if paper_link != None:
+                            if 'http' in paper_link:
+                                links.append(paper_link)
+                            else:
+                                links.append('https://www.sintef.no' + paper_link)
+
+                            line = article.get_text()
+                            a, title, name_line = get_authors(line)
+                            this_paper_authors = []
+                            for single_a in a:
+                                single_a = single_a.replace(" ","")
+                                split = split_by_more(single_a.strip(), [".","-"])
+                                split = [s for s in split if s not in [""," ","  "]]
+                                if len(split)>=2:
+                                    single_a = split[0] + "." + split[-1]
+                                    authors[single_a] = authors.get(single_a,0) + 1
+                                    this_paper_authors.append(single_a)
+
+                            paper_authors.append(this_paper_authors)
+
+                            titles.append(title)
+        
+    x = len(links)
+    succesfull = 0
+
+    for i, (url, a, t) in enumerate(zip(links, paper_authors, titles)):
+        print(f"Working with paper nr. {i+1} out of {x}", end = "\r")
+
+        reader = try_fetching_pdf(url, only_abstract)
+        if reader != None:
+            print("Woho")
+            if type(reader) == type(PdfReader):
+                read_pdfs(reader)
+
+            if type(reader) == type(dict()):
+                pass #GOTTA IMPLEMENT
+
+        # if i%10==0:
+        #     print(f"{succesfull} successfull out of {i+1}", end = "\r")
+        #     time.sleep(5)
+
+try_fetching_pdf('https://doi.org/10.46690/ager.2025.03.04', only_abstract=False)
+# generate_dataset(only_abstract=True)
